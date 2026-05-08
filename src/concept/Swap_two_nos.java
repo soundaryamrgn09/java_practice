e0e6819e225e0637e357ef358e5394084017d7e3
soundarya <soundaryamrgn@gmail.com>
2026-05-08 19:34:30 +0530

    Swap

---
+++ src/concept/Swap_two_nos.java
@@ -0,0 +1,29 @@
+package concept;
+
+import java.util.Scanner;
+
+public class Swap_two_nos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter a : ");
+		int a=sc.nextInt();
+		
+		System.out.println("Enter b : ");
+		int b=sc.nextInt();
+		
+		System.out.println("Before Swapping of two numbers : "+ a +" "+ b);
+		
+		int temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println("After Swapping of two numbers : "+ a +" "+ b);
+		
+
+	}
+
+}
